7ae47918954f207cea771375a159fbd889b7c4c9
dluman <dluman@allegheny.edu>
2022-06-08 22:44:08 -0400

    Updating with interface with response

--- extension.js
+++ extension.js
@@ -4,8 +4,8 @@ const request = require('request');
 
 var user;
 
-let uri = "http://localhost:5000/pollwatcher" // Becomes localhost in implementation
-let urj = "http://localhost:5000/pollreporter" // Becomes localhost in implementation
+let requestURI = "http://localhost:5000/pollwatcher" // Becomes localhost in implementation
+let responseURI = "http://localhost:5000/pollreporter" // Becomes localhost in implementation
 
 let results = {}
 
@@ -22,54 +22,41 @@ const username = () => {
 function activate(context) {
 	username();
 	setInterval(()=>{
-		pollServer(user);
+		let response = request.post({
+			uri: requestURI,
+			body: {"user": user},
+			json: true
+		}, (err, response, body) => {
+			processPrompt(body);
+		});
 	},5000);
-}
-
-/**
- * 
- * @param {String} user 
- */
-function pollServer(user){
-	let response = request.post({
-		uri: uri,
-		body: {"user": user},
-		json: true
-	}, (err, response, body) => {
-		showPrompt(body);
-	});
 }
 
 /**
  * 
  * @param {Object} prompt 
  */
-function showPrompt(prompt){
+function processPrompt(prompt){
 	let message = prompt.message;
 	let votes = prompt.votes;
 	vscode.window.showInformationMessage(
 		message, 
 		...votes
 	).then((vote) => {
-		console.log(votes.indexOf(vote)); // -1 index indicates that poll was closed/timed-out
-		// TO-DO: transmit this data to term-api
-		results[vote] += user;
-		console.log(results)
+		// -1 index indicates that poll was closed/timed-out
+		let result = votes.indexOf(vote); 
+		let transmit = request.post({
+			uri: responseURI,
+			body: {
+				"user": user,
+				"result": result
+			},
+			json:true
+		}, (err, response, body) => {
+			console.log(body);	
+		});
 	});
 }
-
-// NEW function
-function pollResults(results){
-	let resultResponse = request.post({
-		uri: urj,
-		body: {"results": results},
-		json: true
-	}, (err, resultResponse, body) => {
-		console.log(body);
-	});
-}
-
-//function showResults(results) {}
 
 function deactivate() {}
 
